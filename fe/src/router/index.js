b3148533f7a17d2783c0a80eadb38b8f10acd0b9
Emanuel Torres <etorres@rdscr.com>
2026-03-16 16:11:35 -0600

    Pantalla de movimientos

--- fe/src/router/index.js
+++ fe/src/router/index.js
@@ -3,10 +3,13 @@ import LoginView from '../views/LoginView.vue'
 
 import DashboardView from '../views/Dashboard.vue'
 
+import MovimientosView from '../views/Movimientos.vue'
+
 const routes = [
   { path: '/', redirect: '/login' },
   { path: '/login', component: LoginView },
-  { path: '/dashboard', component: DashboardView }
+  { path: '/dashboard', component: DashboardView },
+  { path: '/movimientos', component: MovimientosView }
 ]
 
 const router = createRouter({
