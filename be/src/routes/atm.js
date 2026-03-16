59415b16b181842ef569c6a61b791d870058fe88
Emanuel Torres <etorres@rdscr.com>
2026-03-16 12:30:49 -0600

    endpoint de deposito

--- be/src/routes/atm.js
+++ be/src/routes/atm.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const atmController = require('../controllers/atmController');
 
 router.post('/withdraw', atmController.withdraw);
+router.post('/deposit', atmController.deposit);
 
 module.exports = router;
