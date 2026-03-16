29afd38c3226e8968c36cef7fc7bbb4f06061b50
Emanuel Torres <etorres@rdscr.com>
2026-03-16 15:53:39 -0600

    dashboard principal

--- fe/src/router/index.js
+++ fe/src/router/index.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '../views/LoginView.vue'
 
+import DashboardView from '../views/Dashboard.vue'
+
 const routes = [
   { path: '/', redirect: '/login' },
   { path: '/login', component: LoginView },
-  { path: '/dashboard', component: { template: '<div>Dashboard Placeholder (En Construcción)</div>' } }
+  { path: '/dashboard', component: DashboardView }
 ]
 
 const router = createRouter({
